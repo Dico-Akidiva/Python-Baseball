af696362d25477761ad42761f5a8af62265732cd
DICO AKIDIVA <dicoakidiva@gmail.com>
2020-11-21 18:20:25 +0300

    Completed module

---
+++ stats/data.py
@@ -0,0 +1,35 @@
+import os
+
+import glob
+
+
+import pandas as pd
+
+game_files = glob.glob(os.path.join(os.getcwd(), 'games', '*.EVE'))
+
+list.sort(game_files)
+
+game_frames = []
+
+for game_file in game_files:
+  game_frame == pd.read_csv(game_file, names=('type','multi2', 'multi3', 'multi4', 'multi5', 'multi6', 'event'))
+
+  game_frames.append(game_frame)
+
+games = pd.concat(game_frames)
+
+games.loc['??', ['multi5']] = ''
+
+identifiers = games['multi2'].str.extract(r'(.LS(\D{4})\D{5})')
+
+identifiers = fillna(identifiers, method='ffill')
+
+identifiers.columns(['game_id', 'year'])
+
+games = pd.concat([games, identifiers], axis=1, sort=False)
+
+games = fillna(' ')
+
+games = pd.Categorical(games.loc[type : ])
+
+print(games.head())
